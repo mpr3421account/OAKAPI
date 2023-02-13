73a55c108733f27576f6a4cabe891b4cb6248083
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-13 09:33:16 -0300

    Add project files.

---
+++ OAKAPI/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OAKAPI.Model
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Adress { get; set; }
+
+
+    }
+}
